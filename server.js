9ea6a467dd3eba7fb3072b300a3e8399a38f2643
Chih-Wei <chihweiliu1993@gmail.com>
2018-11-24 13:04:42 +0100

    added save ZIP functionality client-side

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hbs = require('hbs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const port = 4000;
 
 var app = express();
@@ -17,44 +18,3 @@ app.use(express.static(`public`)); // middleware that sets up static directory i
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
-
-
-app.post("/addURL", urlencodedParser, (req, res) => {
-
-  let data = JSON.parse(req.body.data);
-  let folderName = req.body.folderName;
-
-  if (!fs.existsSync(`./${folderName}`)){
-        fs.mkdirSync(`./${folderName}`);
-      }
-
-
-  data.forEach(word => {
-    let id = word[1];
-    let text = word[0];
-    let index = data.indexOf(word) + 1;
-
-    rp(`https://api.soundoftext.com/sounds/${id}`, { json: true }, (err, res, body) => {
-      if (err) { return console.log(err); }
-
-      let url = body.location;
-
-      if(!url) {
-        console.log(`URL for ${text} is missing`)
-      }
-
-
-      rp(url, (err, res, body) => {
-        if (err) { return console.log(err); }
-        console.log(`Saving word: ${text} in folder: ${folderName}`);
-
-
-      })
-      .pipe(fs.createWriteStream(`./${folderName}/${index}-${text}.mp3`));
-
-    });
-  })
-
-  res.send();
-
-})
